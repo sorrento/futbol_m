5ad5434e1b92802874bf2ff39212f239c55f573e
MILENKO <369-90077310@users.noreply.gitlab.example.com>
2022-11-26 18:41:23 +0100

    incluimos histórico de jornadas

--- futbol matias.py
+++ futbol matias.py
@@ -13,96 +13,114 @@
 #     name: python3
 # ---
 
-# primero actualizar el excel que lee de las página web
+# Primero actualizar el excel que lee de las páginas web
 
 # +
 import pandas as pd
 import numpy as np
-from  openpyxl import load_workbook
+from openpyxl import load_workbook
 
-path='data_in/Resultados futbol matías.xlsx'
-wb = load_workbook(filename = path) 
-s=wb.sheetnames
+path = 'data_in/Resultados futbol matías.xlsx'
+wb = load_workbook(filename=path)
+s = wb.sheetnames
 # -
 
-acc=pd.DataFrame()
+acc = pd.DataFrame()
 for name in s:
     try:
-        df=pd.read_excel(path,sheet_name=name)
-        
-        display(df)
-        
-        fecha=df.columns[-1][:-1]
-        jornada=df.columns[0].split(' ')[1]
-        print(jornada)
-        df=df.iloc[:,[0,2,4,6]].copy()
-        df.columns=['Local', 'goles_l', 'goles_v', 'Visita']
-        df['fecha']=fecha
-        df['jornada']=int(jornada)
-        acc=pd.concat([acc, df])
+        df = pd.read_excel(path, sheet_name=name)
+        fecha = df.columns[-1][:-1]
+        jornada = df.columns[0].split(' ')[1]
+        df = df.iloc[:, [0, 2, 4, 6]].copy()
+        df.columns = ['Local', 'goles_l', 'goles_v', 'Visita']
+        df['fecha'] = fecha
+        df['jornada'] = int(jornada)
+        acc = pd.concat([acc, df])
     except:
         print('fail:', name)
 
-acc=acc.dropna().copy()
+acc = acc.dropna().copy()
+acc['dif'] = acc.goles_l - acc.goles_v
 acc
 
-equipos=sorted(list(set(list(acc.Local)+list(acc.Visita))))
+equipos = sorted(list(set(list(acc.Local) + list(acc.Visita))))
 equipos
 
 # # Creación de la matriz de diferencias
 
 # +
-acc['dif']=acc.goles_l-acc.goles_v
+names = ['Local', 'Visita', 'dif', 'fecha', 'jornada']
 
-acc2=acc.copy() #invertimos local y visita
-acc2=acc2.rename(columns={'Local':'Visita', 'Visita':'Local'})
-acc2['dif']=-acc2.dif
-names=['Local', 'Visita', 'dif', 'fecha']
-acct=pd.concat([acc, acc2])[names]
-acct
+acc2 = acc.copy()  # invertimos local y visita
+acc2 = acc2.rename(columns={'Local': 'Visita', 'Visita': 'Local'})
+acc2['dif'] = -acc2.dif
 
-acct['vic']=np.where(acct.dif>0,1,0)
-acct['ptos']=np.where(acct.dif>0,3,np.where(acct.dif<0,0,1))
+acct = pd.concat([acc, acc2])[names]
+acct['vic'] = np.where(acct.dif > 0, 1, 0)
+acct['ptos'] = np.where(acct.dif > 0, 3, np.where(acct.dif < 0, 0, 1))
 acct.reset_index(drop=True)
 acct
 # -
 
-ran=acct.groupby('Local').agg({'dif':'mean', 'vic':'mean', 'ptos':'sum'}).sort_values(['ptos', 'dif'], ascending=False)
-ran['r']=range(1,len(ran)+1)
-ran['name_ran']= list(ran.reset_index().apply(lambda row: str(row.r).zfill(2)+'_'+row.Local, axis=1))
-ran
+jornadas = acct.jornada.drop_duplicates()
 
 # +
-#sesgo: calculemos la media del ranking (puntos) de los rivales de cada equipo
+# ranking usando la información hasta la jornada j
+ran_acc = pd.DataFrame()
+di = {}
 
-ptos_rival=acct[['Local','Visita']].merge(ran.reset_index()[['Local', 'ptos']].rename(columns={'Local':'Visita'}),
-                               on='Visita')
-ptos_rival=ptos_rival.groupby('Local').mean().sort_values('ptos', ascending=False).rename(columns={'ptos':'ptos_rival'})
-ran=ran.join(ptos_rival)
+for j in jornadas:
+    acu = acct[acct.jornada <= j]
+    ran = acu.groupby('Local').agg({'dif': 'mean', 'vic': 'mean', 'ptos': 'sum'}).sort_values(['ptos', 'dif'],
+                                                                                              ascending=False)
+    ran['r'] = range(1, len(ran) + 1)
+    ran['name_ran'] = list(ran.reset_index().apply(lambda row: str(row.r).zfill(2) + '_' + row.Local, axis=1))
+    ran['jornada'] = j
+
+    # sesgo: calculemos la media del ranking (puntos) de los rivales de cada equipo
+    ptos_rival = acu[['Local', 'Visita']].merge(
+        ran.reset_index()[['Local', 'ptos']].rename(columns={'Local': 'Visita'}),
+        on='Visita')
+    ptos_rival = ptos_rival.groupby('Local').mean().sort_values('ptos', ascending=False).rename(
+        columns={'ptos': 'ptos_rival'})
+    ran = ran.join(ptos_rival)
+
+    dic_names = ran[['name_ran']].T.to_dict('records')[0]
+
+    di[j] = dic_names
+    ran_acc = pd.concat([ran_acc, ran])
 # -
 
-ran
+ran_acc
 
-dic_names=ran[[ 'name_ran']].T.to_dict('records')[0]
+buf = pd.DataFrame().from_dict(di, orient='index').reset_index().melt(id_vars='index', value_name='name_ra',
+                                                                      var_name='equipo').drop_duplicates().rename(
+    columns={'index': 'jornada'})
+buf
 
-# Matriz# con nombres con ranking (para pintar la matriz por el mismo orden)
-acct['Local_ran']=[dic_names[x] for x in acct.Local]
-acct['Visita_ran']=[dic_names[x] for x in acct.Visita]
+def popo(acct, buf, x):
+    return acct.merge(buf, left_on=['jornada', x], right_on=['jornada', 'equipo']).drop(columns='equipo').rename(
+        columns={'name_ra': f'{x}_ran'})
 
-# acct.to_csv('matriz.csv', sep=';')
-acct.to_excel('data.xlsx',index=False)
 
+acct = popo(acct, buf, 'Local')
+acct = popo(acct, buf, 'Visita')
+acct = acct.sort_values(['jornada', 'Local_ra'], ascending=[False, True])
+
+acct
+
+acct.to_excel('data.xlsx', index=False)
 ran.to_excel('data_ranking.xlsx', index=True)
 
 # partidos de un equipo
-acc[acc.Visita=='JUNIOR,C.F. F']
+acc[acc.Visita == 'JUNIOR,C.F. F']
 
 # ## otro
 
-loc=acc.groupby('Local').mean().rename(columns={'goles_l':'g_f_L', 'goles_v':'g_c_L'})
-vis=acc.groupby('Visita').mean().rename(columns={'goles_v':'g_f_V', 'goles_l':'g_c_V'})
+loc = acc.groupby('Local').mean().rename(columns={'goles_l': 'g_f_L', 'goles_v': 'g_c_L'})
+vis = acc.groupby('Visita').mean().rename(columns={'goles_v': 'g_f_V', 'goles_l': 'g_c_V'})
 
-goles=loc.join(vis)
+goles = loc.join(vis)
 goles
 
 goles.to_excel('goles.xlsx')
@@ -121,18 +139,18 @@ goles.to_excel('goles.xlsx')
 # No# consideramos local o visita
 
 # +
-acc['dif']=acc.goles_l-acc.goles_v
+acc['dif'] = acc.goles_l - acc.goles_v
 
-pares=acc[['Local', 'Visita', 'dif']].copy()
-pares #positivo es que gana el local
+pares = acc[['Local', 'Visita', 'dif']].copy()
+pares  # positivo es que gana el local
 
 # varis=['x_'+str(i) for i in range(len(equipos))]
-varis=[i for i in range(len(equipos))]
+varis = [i for i in range(len(equipos))]
 
-di=dict(zip(equipos, varis))
+di = dict(zip(equipos, varis))
 
-pares['xl']=pares.apply(lambda x: di[x.Local], axis=1)
-pares['xv']=pares.apply(lambda x: di[x.Visita], axis=1)
+pares['xl'] = pares.apply(lambda x: di[x.Local], axis=1)
+pares['xv'] = pares.apply(lambda x: di[x.Visita], axis=1)
 
 pares
 
@@ -146,16 +164,17 @@ pares
 
 # +
 import numpy as np
-n=len(equipos) 
-  
-arr = np.full((n,n), np.nan)
-arr2 = np.full((n,n), np.nan)
 
-for _,row in pares.iterrows():
-    arr[row.xl,row.xv]=row.dif
-    
-    gana=np.where(row.dif>0,1,0)
-    arr2[row.xl,row.xv]=gana
+n = len(equipos)
+
+arr = np.full((n, n), np.nan)
+arr2 = np.full((n, n), np.nan)
+
+for _, row in pares.iterrows():
+    arr[row.xl, row.xv] = row.dif
+
+    gana = np.where(row.dif > 0, 1, 0)
+    arr2[row.xl, row.xv] = gana
 # -
 
 pd.DataFrame(arr)
@@ -163,74 +182,73 @@ pd.DataFrame(arr)
 pd.DataFrame(arr2)
 
 # +
-m_fila=np.nanmean(arr, axis=1)
-m_col=np.nanmean(arr, axis=0)
+m_fila = np.nanmean(arr, axis=1)
+m_col = np.nanmean(arr, axis=0)
 
-m_fila2=np.nanmean(arr2, axis=1)
-m_col2=np.nanmean(arr2, axis=0)
+m_fila2 = np.nanmean(arr2, axis=1)
+m_col2 = np.nanmean(arr2, axis=0)
 # -
 
 for i in range(n):
     for j in range(n):
-        if np.isnan(arr[i,j]):
-            arr[i,j]=(m_fila[i]+m_col[j])/2
-            #probs de victoria
-            arr2[i,j]=(m_fila2[i]+m_col2[j])/2
-
+        if np.isnan(arr[i, j]):
+            arr[i, j] = (m_fila[i] + m_col[j]) / 2
+            # probs de victoria
+            arr2[i, j] = (m_fila2[i] + m_col2[j]) / 2
 
 pd.DataFrame(arr2)
 
 
 def get_name(pat):
-    li=[x for x in equipos if pat.upper() in x]
-    if len(li)==0:
-        print ('error')
-        res=''
+    li = [x for x in equipos if pat.upper() in x]
+    if len(li) == 0:
+        print('error')
+        res = ''
     else:
-        res=li[0]
+        res = li[0]
     return res
 
 
 def predice(eq1, eq2):
-    e1=get_name(eq1)
-    e2=get_name(eq2)
+    e1 = get_name(eq1)
+    e2 = get_name(eq2)
 
-    i,j=di[e1], di[e2]
+    i, j = di[e1], di[e2]
 
-    print( f'***** {e1} vs {e2}\n')
+    print(f'***** {e1} vs {e2}\n')
 
-    d=round(arr[i,j],1)
-    prob=round(arr2[i,j],2)
-    win=np.where(d>0, e1,e2)
-    print( f'{e1} vs {e2} | gana: {win} por {abs(d)} | prob win local: {prob}')
+    d = round(arr[i, j], 1)
+    prob = round(arr2[i, j], 2)
+    win = np.where(d > 0, e1, e2)
+    print(f'{e1} vs {e2} | gana: {win} por {abs(d)} | prob win local: {prob}')
 
-    d=round(arr[j,i],1)
-    prob=round(arr2[j,i],2)
-    win=np.where(d>0, e2,e1)
-    print( f'{e2} vs {e1} | gana: {win} por {abs(d)} | prob win local: {prob}')
+    d = round(arr[j, i], 1)
+    prob = round(arr2[j, i], 2)
+    win = np.where(d > 0, e2, e1)
+    print(f'{e2} vs {e1} | gana: {win} por {abs(d)} | prob win local: {prob}')
 
 
-predice('cugat', 'egara') 
+predice('cugat', 'egara')
 
-predice('cugat', 'juan') 
+predice('cugat', 'juan')
 
-predice('cugat', 'farga') 
+predice('cugat', 'farga')
 
-predice('cugat', 'pueblo') 
+predice('cugat', 'pueblo')
 
-predice('cugat', 'cavall') 
+predice('cugat', 'cavall')
 
-predice('cugat', 'aran') 
+predice('cugat', 'aran')
 
-predice('cugat', 'septi') 
+predice('cugat', 'septi')
 
-predice('cugat', 'junio') 
+predice('cugat', 'junio')
 
-predice('cugat', 'europa') #j10
+predice('cugat', 'europa')  # j10
 
-predice('cugat', 'oli') #j11
+predice('cugat', 'oli')  # j11
 
-predice('cugat', 'boada') #j12
+predice('cugat', 'boada')  # j12
 
 # +
-#puede haber sesgo porque los de local han tocado más difíciles
+# puede haber sesgo porque los de local han tocado más difíciles
